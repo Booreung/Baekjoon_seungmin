e0ebadc9602ad3fb988b8031e51689a84cf13f85
Booreung <120548686+Booreung@users.noreply.github.com>
2023-04-26 22:23:28 +0900

    [Bronze III] Title: 공 넣기, Time: 44 ms, Memory: 31256 KB -BaekjoonHub

---
+++ 백준/Bronze/10810. 공 넣기/공 넣기.py
@@ -0,0 +1,14 @@
+N_basket, M_method = map(int, input().split())
+
+basket_array = []
+for t in range(N_basket):
+    basket_array.append(0)
+
+
+for t in range(M_method):
+    i, j, k = map(int, input().split())
+    for _ in range(i-1, j):
+        basket_array[_] = k
+
+for t in range(len(basket_array)):
+    print(basket_array[t],end=" ")
