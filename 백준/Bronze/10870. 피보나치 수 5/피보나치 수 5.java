8a066dd7dd4742c16ff141b58277dd242aeacf8b
Booreung <120548686+Booreung@users.noreply.github.com>
2024-03-09 19:14:33 +0900

    [Bronze II] Title: 피보나치 수 5, Time: 208 ms, Memory: 17624 KB -BaekjoonHub

---
+++ 백준/Bronze/10870. 피보나치 수 5/피보나치 수 5.java
@@ -0,0 +1,17 @@
+import java.util.*;
+
+public class Main {
+	
+	static int fibo(int n) {
+		if(n==0) return 0;
+		if(n==1) return 1;
+		return fibo(n-1)+fibo(n-2);
+	}
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int num = sc.nextInt();
+		
+		System.out.println(fibo(num));
+	}
+}
